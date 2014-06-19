f182817db4e75fd128ce1a0046e678e837414278
Silvio Traversaro <silvio.traversaro@iit.it>
2014-06-19 12:31:43 +0200

    Converted functions intended to be used at runtime to use numerid ids

--- include/wbi/iWholeBodyActuators.h
+++ include/wbi/iWholeBodyActuators.h
@@ -24,7 +24,7 @@
 namespace wbi {
     class wbiId;
     class wbiIdList;
-    
+
     /**
      * Interface to the actuators of the robot.
      */
@@ -34,26 +34,26 @@ namespace wbi {
         virtual ~iWholeBodyActuators();
         virtual bool init() = 0;
         virtual bool close() = 0;
-        
+
         //virtual int getActuatorNumber() = 0;
         virtual bool removeActuator(const wbiId &j) = 0;
         virtual bool addActuator(const wbiId &j) = 0;
         virtual int addActuators(const wbiIdList &j) = 0;
         virtual const wbiIdList& getActuatorList() = 0;
-        
+
         /** Set the control mode of the specified joint(s).
          * @param controlMode Id of the control mode.
          * @param ref Reference value(s) for the controller.
          * @param joint Joint number, if negative, all joints are considered.
          * @return True if operation succeeded, false otherwise. */
         virtual bool setControlMode(ControlMode controlMode, double *ref=0, int joint=-1) = 0;
-        
+
         /** Set the reference value for the controller of the specified joint(s).
          * @param ref Reference value(s) for the controller.
          * @param joint Joint number, if negative, all joints are considered.
          * @return True if operation succeeded, false otherwise. */
         virtual bool setControlReference(double *ref, int joint=-1) = 0;
-        
+
         /** Set a parameter (e.g. a gain) of one or more joint controllers.
          * @param paramId Id of the parameter.
          * @param value Value(s) of the parameter.
@@ -61,7 +61,7 @@ namespace wbi {
          * @return True if operation succeeded, false otherwise. */
         virtual bool setControlParam(ControlParam paramId, const void *value, int joint=-1) = 0;
     };
-    
+
 }
 
 #endif //IWHOLEBODYACTUATORS_H
